8746186e640cd5532240ec348c07368698054573
Olivier Fisette <ofisette@gmail.com>
2026-01-20 15:51:18 -0500

    Améliorer le script pour l’exercice sur les vecteurs de tâches
    Avec deux `echo`, avant et après `sleep`, et un commentaire, on comprend
    mieux le flux de chaque tâche.

--- lab/hello-array/hello-job.sh
+++ lab/hello-array/hello-job.sh
@@ -3,6 +3,6 @@
 #SBATCH --job-name=hello
 #SBATCH --array=1-10
 
-echo "Hello from index $SLURM_ARRAY_TASK_ID"
-
-sleep 60
+echo "Starting job array index $SLURM_ARRAY_TASK_ID"
+sleep 60 # Simulate a 60-second computation
+echo "Finished job array index $SLURM_ARRAY_TASK_ID"
